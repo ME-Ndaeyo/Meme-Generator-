b0f5776579a4f5d6c2faa39eab254b99d9ddbcc9
Bobbiie <mmeyenendaeyo@gmail.com>
2023-02-28 17:23:07 +0100

    fix: Fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Box from "./components/Box";
 import Meme from "./components/Meme";
 import Navbar from "./components/Navbar";
-import boxes from "./boxes";
 
-export default function Card() {
-  const [square, setSquare] = useState(boxes);
-
-  // function toggle(id) {
-  //   setSquare((prevState) => {
-  //     return prevState.map(el =>{
-  //       return el.id === id? {...el, on: !el.on} : el
-  //     })
-  //   });
-  // }
-  
-  function toggle(id) {
-    setSquare((prevState) => {
-      return prevState.map(el =>{
-        return el.id === id? {...el, on: !el.on} : el
-      })
-    });
-  }
-
-  const boxEl = square.map((el) => <Box key={el.id} id={el.id} on={el.on} click={toggle} />);
-
+export default function Card() {;
   return (
     <section>
       <Navbar />
       <Meme/>
-      <div className="box-container">
-        {boxEl}
-      </div>
     </section>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
 import memeData from "../memeData";
 
 export default function Meme() {
@@ -8,30 +9,67 @@ export default function Meme() {
     randomImage: "https://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemesImage, setAllMemesImage] = React.useState(memeData);  
+  const [allMemesImage, setAllMemesImage] = React.useState(memeData);
+  // const [api, setApi] = React.useState({});
+
+  useEffect(
+    function callApi(){
+      fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json())
+      .then((data) => setAllMemesImage(data.data.memes))
+      .catch((err) => console.log(`Error ${err}`));
+    }
+  , [])
+  console.log(allMemesImage)
 
   function getMemeImage(e) {
     e.preventDefault();
-    const rand = Math.trunc(Math.random() * allMemesImage.data.memes.length);
-    const url = allMemesImage.data.memes[rand].url; 
+    const rand = Math.trunc(Math.random() * allMemesImage.length);
+    const url = allMemesImage[rand].url;
 
-    setMeme(prevMemeState => ({
-        ...prevMemeState,
-        randomImage: url
+    setMeme((prevMemeState) => ({
+      ...prevMemeState,
+      randomImage: url,
+    }));
+    // callApi()
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
     }));
   }
 
   return (
     <main>
       <form>
-        <input type="text" className="input--top" />
-        <input type="text" className="input--bottom" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="input--top"
+          onChange={handleChange}
+          value={meme.topText}
+          name="topText"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="input--bottom"
+          onChange={handleChange}
+          value={meme.bottomText}
+          name="bottomText"
+        />
         <button type="submit" onClick={getMemeImage}>
           Get a new meme image
         </button>
       </form>
       <div className="meme--img-box">
-        <img src={`${meme.randomImage}`} alt="meme" />
+        <img src={meme.randomImage} alt="meme" className="meme--img" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
